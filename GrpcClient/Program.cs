d4931c9a9455f1689dfb8750526db312ae08545f
Shaun Evans <shaunevans@gmail.com>
2024-01-06 00:26:49 +0000

    adding new product info proto and calling logig in client application

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,11 +1,16 @@
 ﻿using Grpc.Net.Client;
-using GrpcClient;
+using GrpcClient.Protos;
 
 // The port number must match the port of the gRPC server.
 using var channel = GrpcChannel.ForAddress("https://localhost:7288");
-var client = new Greeter.GreeterClient(channel);
-var reply = await client.SayHelloAsync(
-                  new HelloRequest { Name = "Shaun Evans" });
-Console.WriteLine("Greeting: " + reply.Message);
+
+var greeterClient = new Greeter.GreeterClient(channel);
+var greeterReply = await greeterClient.SayHelloAsync(request: new HelloRequest { Name = "Shaun Evans" });
+Console.WriteLine("Greeting: " + greeterReply.Message);
+
+var productClient = new Product.ProductClient(channel);
+var productReply = await productClient.GetProductsInformationAsync(request: new GetProductDetail { ProductId = 3 });
+Console.WriteLine($"{productReply.ProductName} | {productReply.ProductDescription} | {productReply.ProductPrice} | {productReply.ProductStock}");
+
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
