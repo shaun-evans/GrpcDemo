075c486ff461af6e66bdc837c96f7c5db49bb77d
Shaun Evans <shaunevans@hotmail.com>
2024-01-25 16:10:53 +0000

    refactored program.cs in client app

---
+++ GrpcClient/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace GrpcClient
+{
+    class Configuration
+    {
+        public string ServerAddress { get; }
+
+        public Configuration()
+        {
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var configuration = configBuilder.Build();
+            ServerAddress = configuration["ServerAddress"];
+        }
+    }
+}
--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,16 +1,35 @@
 ﻿using Grpc.Net.Client;
+using GrpcClient;
 using GrpcClient.Protos;
+using Microsoft.Extensions.Configuration;
 
-// The port number must match the port of the gRPC server.
-using var channel = GrpcChannel.ForAddress("https://localhost:7288");
+class Program
+{
+    static async Task Main()
+    {
+        var config = new Configuration();
+        var serverAddress = config.ServerAddress;
 
-var greeterClient = new Greeter.GreeterClient(channel);
-var greeterReply = await greeterClient.SayHelloAsync(request: new HelloRequest { Name = "Shaun Evans" });
-Console.WriteLine("Greeting: " + greeterReply.Message);
+        using var channel = GrpcChannel.ForAddress(serverAddress);
 
-var productClient = new Product.ProductClient(channel);
-var productReply = await productClient.GetProductsInformationAsync(request: new GetProductDetail { ProductId = 3 });
-Console.WriteLine($"{productReply.ProductName} | {productReply.ProductDescription} | {productReply.ProductPrice} | {productReply.ProductStock}");
+        await Greet(channel, "Shaun Evans");
+        await GetProductDetails(channel, 3);
 
-Console.WriteLine("Press any key to exit...");
-Console.ReadKey();
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+    private static async Task Greet(GrpcChannel channel, string name)
+    {
+        var greeterClient = new Greeter.GreeterClient(channel);
+        var greeterReply = await greeterClient.SayHelloAsync(request: new HelloRequest { Name = name });
+        Console.WriteLine("Greeting: " + greeterReply.Message);
+    }
+
+    private static async Task GetProductDetails(GrpcChannel channel, int productId)
+    {
+        var productClient = new Product.ProductClient(channel);
+        var productReply = await productClient.GetProductsInformationAsync(request: new GetProductDetail { ProductId = productId });
+        Console.WriteLine($"{productReply.ProductName} | {productReply.ProductDescription} | {productReply.ProductPrice} | {productReply.ProductStock}");
+    }
+}
