e12719ef5690c27ba78d6db1801cc5c9727216cd
Shaun Evans <shaunevans@hotmail.com>
2024-01-25 16:13:44 +0000

    refactoring service program.cs

--- GrpcService/Program.cs
+++ GrpcService/Program.cs
@@ -1,18 +1,30 @@
 using GrpcService.Services;
 
+const string gRPCClientCreationUrl = "https://go.microsoft.com/fwlink/?linkid=2086909";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit macOSConfigurationUrl
 
 // Add services to the container.
-builder.Services.AddGrpc();
+RegisterServices(builder.Services);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGrpcService<ProductService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+ConfigureRequestPipeline(app);
 
 app.Run();
+
+void RegisterServices(IServiceCollection services)
+{
+    services.AddGrpc();
+}
+
+void ConfigureRequestPipeline(WebApplication app)
+{
+    app.MapGrpcService<GreeterService>();
+    app.MapGrpcService<ProductService>();
+    app.MapGet("/", () => $"Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: {gRPCClientCreationUrl}");
+}
